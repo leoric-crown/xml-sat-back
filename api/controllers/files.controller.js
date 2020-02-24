e80aba885d537c8560fcf84f4f15906b754e9227
LEON TREVIÑO <l.leon@cimmyt.org>
2020-02-23 20:31:30 -0600

    Implemented /files/ endpoint. Can now receive files from clients.

--- api/controllers/files.controller.js
+++ api/controllers/files.controller.js
@@ -1,18 +1,31 @@
 const config = require('../../config/main')
 const endpoint = `${config.host}/encounters/`
+const { IncomingForm } = require('formidable')
 
 const unzipFile = async (req, res) => {
     console.log('REACHED UNZIP ENDPOINT')
-    console.log(req.files)
-    try {
-        res.status(200).json({
-            message: 'Welcome to unzip endpoint',
-            time: Date.now()
-        })
-    }
-    catch (err) {
+    console.log(req)
 
-    }
+    const form = new IncomingForm()
+    
+    form.on('fileBegin', (field, file) => res.status(200).json({
+        message: 'File received',
+        fileName: file.name,
+        time: Date.now()
+    }))
+
+    form.on('file', (field, file) => {
+        // console.log({ field, file })
+    })
+
+
+    // form.on('end', () => res.status(200).json({
+    //     message: 'Form received',
+    //     fileName: receivedFile.name,
+    //     time: Date.now()
+    // }))
+
+    form.parse(req)
 }
 
 module.exports = {
